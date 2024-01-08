0ca69ded4a9fb50e2116794b4d2b3b1f3ee9d166
Kumar209 <76429886+Kumar209@users.noreply.github.com>
2024-01-08 14:24:59 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: 'Home/',
-=======
-  base: '/home/',
->>>>>>> 4644cf333f636bd40f5a509283015e207067d67f
+  base : '/vite/',
 })
